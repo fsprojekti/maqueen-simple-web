07f1ab30137ff46fe6fa2fa8ed00cf8b4f0f16c6
tomazpozrl <tomaz.pozrl@fs.uni-lj.si>
2023-09-20 15:10:21 +0200

    added html input for robot car ip

--- arduino.ino
+++ arduino.ino
@@ -5,10 +5,9 @@
 DFRobot_MaqueenPlus maqueenPlus(22, 21);
 
 const char* ssid = "TP-LINK_0B69";
-const char* password = "29295820";
+const char* password = "29205820";
 
 WebSocketsServer webSocket(81);
-
 
 void handleCommand(const String& command, int speed) {
     Serial.printf("Command: %s, Speed: %d\n", command.c_str(), speed);
@@ -45,8 +44,6 @@ void handleCommand(const String& command, int speed) {
     }
 }
 
-
-
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
     switch (type) {
         case WStype_DISCONNECTED:
@@ -76,6 +73,8 @@ void setup() {
     Serial.println("Connecting to WiFi...");
   }
   Serial.println("Connected to WiFi.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 
   webSocket.begin();
   webSocket.onEvent(webSocketEvent);
@@ -91,96 +90,3 @@ void setup() {
 void loop() {
   webSocket.loop();
 }
-
-
-
-// #include <LiteLED.h>
-
-// #define LED_TYPE LED_STRIP_WS2812
-// #define LED_GPIO 23
-// #define
-
-// LiteLED ledUnder(LED_TYPE, 0);
-
-
-
-// static const crgb_t L_RED = 0xff0000;
-// static const crgb_t L_GREEN = 0x00ff00;
-// static const crgb_t L_BLUE = 0x0000ff;
-// static const crgb_t L_WHITE = 0xe0e0e0;
-
-// int sensorSum = 0;
-// int sensorSumMax = 1000;
-
-
-// void setup() {
-//   // initialize serial print
-//   Serial.begin(115200);
-
-//   Serial.println("initialize MaqueenPlus I2C communication");
-//   while (maqueenPlus.begin() != 0) {
-//     Serial.println("I2C initialization failed");
-//   }
-//   Serial.println("I2C initialization success");
-
-//   //Turn on lights under the robot
-//   ledUnder.begin(LED_GPIO, 4);  // initialze the myLED object. Here we have 1 LED attached to the LED_GPIO pin
-//   ledUnder.brightness(100);     // set the LED photon intensity level
-//   ledUnder.setPixel(0, L_WHITE, 1);
-//   delay(200);
-//   ledUnder.setPixel(1, L_WHITE, 1);
-//   delay(200);
-//   ledUnder.setPixel(2, L_WHITE, 1);
-//   delay(200);
-//   ledUnder.setPixel(3, L_WHITE, 1);
-//   delay(200);
-// }
-
-// int counterA = 0;
-// int counterB = 0;
-// void loop() {
-//   if (counterA > 10) {
-//     counterA = 0;
-//     uint16_t* state = maqueenPlus.getGrayscale();
-//     sensorSum = state[0] + state[1] + state[2];
-//     if (sensorSumMax<sensorSum) sensorSumMax=sensorSum;
-//   }
-//   if (counterB > 200) {
-//     counterB = 0;
-//     Serial.print(sensorSum);
-//     Serial.print(" : ");
-//     Serial.println(sensorSumMax);
-//   }
-//   delay(1);
-//   counterA++;
-//   counterB++;
-
-
-//   //Move robot
-//   // Serial.print(maqueenPlus.eALL);
-//   // Serial.print(" ");
-//   // Serial.print(maqueenPlus.eCW);
-//   // Serial.println();
-
-//   // Serial.print("Max sensor: ");
-//   // Serial.print(sensorSumMax);
-//   // Serial.print(" current sum: ");
-//   // Serial.println(sensorSum);
-//   // delay(100);
-//   // Serial.println("test");
-//   // maqueenPlus.getGrayscale();
-
-//   // uint8_t dataOn[] = { 1 };
-//   // uint8_t dataOff[] = { 0 };
-//   // maqueenPlus.writeReg(0x0B, dataOn, 1);
-//   // delay(1000);
-//   // maqueenPlus.writeReg(0x0B, dataOff, 1);
-//   // delay(1000);
-
-//   // uint8_t dataOn[] = {1,50};
-//   // uint8_t dataOff[] = {2, 50};
-//   // maqueenPlus.writeReg(0x00, &dataOn[0], 2);
-//   // delay(1000);
-//   // maqueenPlus.writeReg(0x00, &dataOff[0], 2);
-//   // delay(1000);
-// }
